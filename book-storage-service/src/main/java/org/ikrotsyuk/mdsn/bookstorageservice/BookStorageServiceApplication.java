2099bf10e22f75c2ad3aeddfeb2e51ebb00cc87f
IvanKrtsk <ikrotsyuk@gmail.com>
2025-01-16 13:21:50 +0300

    init commit

---
+++ book-storage-service/src/main/java/org/ikrotsyuk/mdsn/bookstorageservice/BookStorageServiceApplication.java
@@ -0,0 +1,12 @@
+package org.ikrotsyuk.mdsn.bookstorageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStorageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStorageServiceApplication.class, args);
+    }
+}
