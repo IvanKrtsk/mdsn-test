ca39af6f53be3b8f10a9daa230efc84869fc63a0
IvanKrtsk <ikrotsyuk@gmail.com>
2025-01-16 16:22:41 +0300

    add and configure api gateway

--- book-storage-service/src/main/java/org/ikrotsyuk/mdsn/bookstorageservice/controller/StorageController.java
+++ book-storage-service/src/main/java/org/ikrotsyuk/mdsn/bookstorageservice/controller/StorageController.java
@@ -1,9 +1,11 @@
 package org.ikrotsyuk.mdsn.bookstorageservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/book-storage")
 public class StorageController {
     @GetMapping("/sayhey")
     public String say(){
