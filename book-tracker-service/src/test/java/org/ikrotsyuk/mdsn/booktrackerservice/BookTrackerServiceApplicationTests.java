bda98eb355e5a4bdad2b6bebda677a805e6827c2
IvanKrtsk <ikrotsyuk@gmail.com>
2025-01-16 17:23:09 +0300

    add book tracker service and its routing

---
+++ book-tracker-service/src/test/java/org/ikrotsyuk/mdsn/booktrackerservice/BookTrackerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ikrotsyuk.mdsn.booktrackerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTrackerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
